c088cb0971da661a23560e28810ebf5b878656a3
John Pow <johnowed@gmail.com>
2023-06-17 20:53:13 -0700

    working on trouble shooting install

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -64,6 +64,8 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            
             },
           },
         },
